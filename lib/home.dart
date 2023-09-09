0aa10890440ae20b424aef98bd216ef4a31f6672
sajal <sajalsahu8@gmail.com>
2023-09-09 18:18:20 +0530

    configured exit operatins

--- lib/home.dart
+++ lib/home.dart
@@ -3,23 +3,35 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios),
-          onPressed: () {
-            SystemNavigator.pop();
-            exit(0);
-          },
-        ),
+        leading: Platform.isAndroid
+            ? IconButton(
+                icon: const Icon(Icons.arrow_back_ios),
+                onPressed: () {
+                  SystemNavigator.pop();
+                },
+              )
+            : IconButton(
+                iconSize: 0,
+                icon: const Icon(Icons.check_box_outline_blank),
+                onPressed: () {
+                  //  Navigator.pop(context);
+                },
+              ),
         backgroundColor: Colors.blueGrey,
         centerTitle: true,
-        title: Text("Add Image/Icon"),
+        title: const Text("Add Image/Icon"),
       ),
       //body: Text("Home Screen"),
     );
