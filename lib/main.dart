aa0e4e0eb94998d26821ff9052e14af2b7705104
sajal <sajalsahu8@gmail.com>
2023-09-09 17:29:14 +0530

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:celebrate/splash.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    title: 'Flutter Tutorial',
-    home: SplashScreen(),
-  ));
+  runApp(
+    MaterialApp(
+      title: 'Flutter Tutorial',
+      home: SplashScreen(),
+    ),
+  );
 }
--- lib/splash.dart
+++ lib/splash.dart
@@ -1,10 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
+import 'package:celebrate/home.dart';
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const HomeScreen())));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color(0xFFFF800B),
+                Color(0xFFCE1010),
+              ]),
+        ),
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: [
+                // Image.asset(
+                //   "assets/images/pizza.png",
+                //   height: 300.0,
+                //   width: 300.0,
+                // ),
+                Text(
+                  "Home Delivery Partner",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.orange),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
